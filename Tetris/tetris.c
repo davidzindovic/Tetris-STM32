d800e2d04575cd8540150db0c79e958222b2d953
David Zindović <96695748+davidzindovic@users.noreply.github.com>
2024-11-17 14:24:14 +0100

    Create tetris.c

---
+++ Tetris/tetris.c
@@ -0,0 +1,21 @@
+
+
+
+//definiraš blocke z dolzinami stranic oz. koordinatami
+
+
+struct block_struct{ //2x3 grid
+  bool block1; //11
+  bool block2; //12
+  bool block3; //13
+  bool block4; //21
+  bool block5; //22
+  bool block6; //23
+}blocks[5]=
+{
+{0,1,0,1,1,1},
+{0,0,1,1,1,1},
+{0,0,0,1,1,1},
+{1,0,0,1,1,1},
+{0,1,1,0,1,1}
+};
